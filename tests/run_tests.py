de191709a446cbb0a0b2fa33c30fc3e40b8dfe57
Tim Holt <atimholt@gmail.com>
2014-04-28 02:37:00 -0700

    small whitespace change, dang nerdcommenter.
    --HG--
    branch : dev

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -20,9 +20,9 @@ with tempfile.NamedTemporaryFile(mode='w', encoding='utf-8') as simple_vimrc:
 	
 	#print(subprocess.check_output(['python', '-c', "\"print('hi')\""]))
 	output = subprocess.check_output(
-			['vim', '-u', simple_vimrc.name, '+Vader*'],
-			stderr=subprocess.STDOUT,
-			shell=True)
+	        ['vim', '-u', simple_vimrc.name, '+Vader*'],
+	        stderr=subprocess.STDOUT,
+	        shell=True)
 	print(output)
 
 # vim: set noet ts=4 sts=4 sw=4 :
